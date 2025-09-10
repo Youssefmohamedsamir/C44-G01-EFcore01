ec60819b91876b97308058500bfb724acc486c5d
Youssef <yosefmohmed533255@gmail.com>
2025-09-10 23:48:34 +0300

    Question02

---
+++ AssignmentQ02/DataText/AirDbContext.cs
@@ -0,0 +1,44 @@
+﻿using AssignmentQ02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQ02.DataText
+{
+    internal class AirDbContext : DbContext
+    {
+        public AirDbContext():base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server =. ; Database = AirDb; Trusted_Connection = true ; TrustServerCertificate = true");
+        }
+
+        public DbSet<Route> Routes { get; set; } 
+
+        public DbSet<Emp_Qualifications> Emp_Qualifications { get; set; }   
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Aircraft_Routes> Aircraft_Routes { get; set; }
+
+        public DbSet<AirCraft> Aircrafts { get; set; } 
+
+        public DbSet<Transaction> Transactions { get; set; }  
+
+        public DbSet<Airline> Airlines { get; set; }
+
+        public DbSet<Airline_Phones> Airline_Phones { get; set; }
+
+
+
+
+    }
+
+    
+}
---
+++ AssignmentQ02/Models/Airline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentQ02.Models
+{
+    internal class Airline
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int Cont_person { get; set; }
+    }
+}
